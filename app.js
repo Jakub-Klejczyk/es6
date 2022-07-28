777d593aff7dec789fa140d99a327e13b9adc532
Jakub <jakub-klejczyk@o2.pl>
2022-07-28 18:09:39 +0200

    zadanie wykonane

--- app.js
+++ app.js
@@ -1,27 +1,46 @@
-const firstName = "Jan";
-const lastName = "Kowalski";
+// function Person(firstName, lastName) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+// }
 
-// const person3 = {
-//     firstName: firstName,
-//     lastname: lastname,
-//     getFullname: function () {
-//       return `Ja to ${this.firstName} ${this.lastName}`;
-//     }
-//   };
+// Person.prototype.sayHello = function () {
+//   return this.firstName + " " + this.lastName;
+// };
 
-const gfnr = "getFullnameReverse";
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  sayHello() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
 
-const person3 = {
-  firstName,
-  lastName,
-  getFullname() {
-    return `Ja to ${this.firstName} ${this.lastName}`;
-  },
-  [gfnr]() {
-    return `Ja to ${this.lastName} ${this.firstName}`;
-  },
-};
+const person1 = new Person("Janeczek", "Kowalski");
 
-console.log(person3);
-console.log(person3.getFullname());
-console.log(person3.getFullnameReverse());
+console.log(person1.sayHello());
+
+//dziedziczenie
+
+class Student extends Person {
+  constructor(firstName, lastName, age = "0") {
+    super(firstName, lastName);
+    this.age = age;
+  }
+
+  // jeśli nie stworze nowego konstruktora to js sam przeniesie nadrzędne parametry przy pomocy spred operatora
+  //   constructor(...args) {
+  //     super(...args);
+  //   }
+
+  sayHello2() {
+    return `Nazywam się ${super.sayHello()} i mam ${this.age} lat.`;
+  }
+}
+
+const student1 = new Student("Mariusz", "Kamiński");
+
+console.log(student1.sayHello2());
+
+// dziedziczyć można również z wbudowanych typów jak np. Array
