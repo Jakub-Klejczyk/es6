a955271b6b7ea6e5c58971fdf99f0d35f50a6658
Jakub <jakub-klejczyk@o2.pl>
2022-07-28 17:31:53 +0200

    zadanie wykonane

--- app.js
+++ app.js
@@ -1,52 +1,27 @@
-const numbers = [1, 2, 3, 4, 5, 6];
+const firstName = "Jan";
+const lastName = "Kowalski";
 
-//const one = numbers[0]
-// const [one] = numbers;
-// console.log(one);
+// const person3 = {
+//     firstName: firstName,
+//     lastname: lastname,
+//     getFullname: function () {
+//       return `Ja to ${this.firstName} ${this.lastName}`;
+//     }
+//   };
 
-const [one, two, , four, ...rest] = numbers;
-console.log(one);
-console.log(two);
-console.log(four);
-console.log(rest);
+const gfnr = "getFullnameReverse";
 
-function działania(a, b) {
-  return [a + b, a * b];
-}
-
-const [suma, mnożenie, dzielenie = "Ni ma!"] = działania(2, 3);
-
-console.log(suma);
-console.log(mnożenie);
-console.log(dzielenie);
-
-const person = {
-  name: "Franek",
-  age: 3,
-  sports: {
-    main: "Sandbox",
-    second: "Kindergarden",
-    third: "Kindermachen",
+const person3 = {
+  firstName,
+  lastName,
+  getFullname() {
+    return `Ja to ${this.firstName} ${this.lastName}`;
+  },
+  [gfnr]() {
+    return `Ja to ${this.lastName} ${this.firstName}`;
   },
 };
 
-const { name: firstName, age, food = "Wszystko", ...sports } = person;
-const {
-  sports: { main },
-} = person;
-
-console.log(firstName);
-console.log(age);
-console.log(food);
-console.log(sports);
-console.log(main);
-
-const person2 = { ...person };
-
-console.log(person2);
-
-function showYa({ name, age }) {
-  console.log(`Nazywam się ${name} i mam ${age} lat`);
-}
-
-showYa(person2);
+console.log(person3);
+console.log(person3.getFullname());
+console.log(person3.getFullnameReverse());
